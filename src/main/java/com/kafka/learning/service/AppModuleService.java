1f9b901ba1623170b68d20f7cc5cbf165a357836
JAY\Laptop <jaykshirsagar6@gmail.com>
2024-11-18 21:39:46 +0200

    Adding kafka

--- src/main/java/com/kafka/learning/service/AppModuleService.java
+++ src/main/java/com/kafka/learning/service/AppModuleService.java
@@ -1,23 +1,31 @@
 package com.kafka.learning.service;
 
 import com.kafka.learning.dto.AppModuleDto;
+import com.kafka.learning.dto.ApplicationDto;
 import com.kafka.learning.mapper.AppModuleMapper;
 import com.kafka.learning.model.AppModule;
 import com.kafka.learning.repository.AppModuleRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class AppModuleService {
 
-    @Autowired
-    AppModuleRepository appModuleRepository;
+    @NonNull
+    private AppModuleRepository appModuleRepository;
+    @NonNull
+    private KafkaTemplate<String, AppModuleDto> kafkaTemplate;
 
     public AppModule save(AppModule appModule) {
         AppModuleDto appModuleDto = AppModuleMapper.INSTANCE.appModuleToAppModuleDto(appModuleRepository.save(appModule));
+        kafkaTemplate.send("module", appModuleDto);
         return appModule;
     }
 
@@ -34,5 +42,10 @@ public class AppModuleService {
 
     public void deleteById(String id) {
         appModuleRepository.deleteById(id);
+    }
+
+    @KafkaListener(topics = "module", groupId = "my-group")
+    public void listen(ApplicationDto applicationDto) {
+        System.out.println("Received message: " + applicationDto);
     }
 }
--- src/main/java/com/kafka/learning/service/ApplicationService.java
+++ src/main/java/com/kafka/learning/service/ApplicationService.java
@@ -7,19 +7,29 @@ import com.kafka.learning.mapper.ApplicationMapper;
 import com.kafka.learning.model.AppModule;
 import com.kafka.learning.model.Application;
 import com.kafka.learning.repository.ApplicationRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
+@RequiredArgsConstructor
 public class ApplicationService {
-    @Autowired
-    ApplicationRepository applicationRepository;
+
+    @NonNull
+    private ApplicationRepository applicationRepository;
+    @NonNull
+    private KafkaTemplate<String, ApplicationDto> kafkaTemplate;
 
     public Application save(Application application) {
         ApplicationDto applicationDto = ApplicationMapper.INSTANCE.applicationToApplicationDto(applicationRepository.save(application));
+        kafkaTemplate.send("application", applicationDto);
         return application;
     }
 
@@ -36,5 +46,10 @@ public class ApplicationService {
 
     public void deleteById(String id) {
         applicationRepository.deleteById(id);
+    }
+
+    @KafkaListener(topics = "application", groupId = "my-group")
+    public void listen(ApplicationDto applicationDto) {
+        System.out.println("Received message: " + applicationDto);
     }
 }
